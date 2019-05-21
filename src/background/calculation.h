4256ac93a53912cc5a84fe8614aa5dc86e04ca7a
marcel <marcel@localhost.localdomain>
2019-05-21 13:59:51 +0200

    added function to get the vector end

--- src/background/calculation.h
+++ src/background/calculation.h
@@ -24,10 +24,12 @@ public:
     void calcValues(vector<Run> reaction_times, int all_trials, int right_trials);
 private:
     // functions for calculation of all values
-    void calcAverageReactionTime(Run *reaction_times);   // calculates the average reaction time of all reaction times, which aren't -1 (no reaction, trials after game quit)
-    double calcMedian(vector<Run> to_median);                   // calculates the median of an vector<int>
+    void calcAverageReactionTime(Run *reaction_times);      // calculates the average reaction time of all reaction times, which aren't -1 (no reaction, trials after game quit)
+    double calcMedian(Run *median);                         // calculates the median of an vector<int>
     void calcRightPercentage(int all_trials, int right_trials); // calculates the percentage of right reactions
     void calcEffects(vector<Run> reaction_times1, vector<Run>reaction_times2);
+    bool compareTimeFunction(Trial& t1, Trial &t2); // a function for sort all Trials in run vector (compare all time values)
+
     // variables - calculated values
     double average;
     double median;

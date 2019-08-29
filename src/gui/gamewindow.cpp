4b8d0f97e08bc601289ad1c8083ebed8bf5207ad
hrafnass <aetas.cieris@posteo.de>
2019-08-29 11:57:30 +0200

    added ESC in keyEvent

--- src/gui/gamewindow.cpp
+++ src/gui/gamewindow.cpp
@@ -5,9 +5,24 @@
 
 using namespace std;
 
+
+// window constructor
 GameWindow::GameWindow(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::GameWindow)
 {
     ui->setupUi(this);
+}
+
+
+
+
+// protected
+// keyEvent
+void GameWindow::keyPressEvent(QKeyEvent *event){
+    /* needed keys:
+     *      ESC - quit the game
+     *      left arrow and right arrow
+     */
+
 }

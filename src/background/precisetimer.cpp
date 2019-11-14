199e64132e6bd9ff235571e0cb9e00330dc00119
hrafnass <aetas.cieris@posteo.de>
2019-11-14 13:48:19 +0100

    added setTimeStamp function

--- src/background/precisetimer.cpp
+++ src/background/precisetimer.cpp
@@ -2,20 +2,22 @@
 
 PreciseTimer::PreciseTimer()
 {
-
+    QueryPerformanceFrequency(&frequency);  // cached the counter frequency
 }
 
-bool PreciseTimer::init(){
+/*bool PreciseTimer::init(){
     // in windows xp and later it shouldn't happens
     if(!QueryPerformanceFrequency(&frequency)){
         cout << "Error: Hardware doesn't support high performance" << endl;
         return false;
     }
-
-    /*if(frequency == 0){
-        cout << "Error: Hardware doesn't support high performance" << endl;
-        return false;
-    }*/
-
     return true;
+}*/
+
+void PreciseTimer::setTimeStamp(bool start_or_stop){
+    QueryPerformanceCounter(&stamp);
+    if(start_or_stop)
+        start = stamp;
+    else
+        stop = stamp;
 }
--- src/background/precisetimer.h
+++ src/background/precisetimer.h
@@ -11,18 +11,26 @@ using namespace std;
 #define STOP  false     // time stamp saved int stop
 
 // Measures with the windows api the time in ms accuracy.
+/* PreciseTimer uses the QueryPerfomanceCounter (QPC) of the Windows API.
+ * The class only can used on Windows XP and higher without doubt.
+ * On lower windows versions it could be that QPC doesn't work.
+ */
 
 class PreciseTimer
 {
 public:
     PreciseTimer();
-    bool init();
-    bool setTimeStamp(bool);
+    // bool init();
+    void setTimeStamp(bool);
     long elapsed();         // calculates the differenz between start and stop and set them to 0
 private:
+    // saves the needed time stamps
     LARGE_INTEGER start;
     LARGE_INTEGER stop;
+    // hold the frequency of the counter in the cache
     LARGE_INTEGER frequency;
+    // stamp for setTimeStamp
+    LARGE_INTEGER stamp;
 };
 
 #endif // PRECISETIMER_H

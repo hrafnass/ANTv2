80a58d55020a4796bbb4aa1bc133f26547ac1a61
hrafnass <aetas.cieris@posteo.de>
2019-11-14 13:30:46 +0100

    added a init function in precisetimer

--- src/background/precisetimer.cpp
+++ src/background/precisetimer.cpp
@@ -4,3 +4,18 @@ PreciseTimer::PreciseTimer()
 {
 
 }
+
+bool PreciseTimer::init(){
+    // in windows xp and later it shouldn't happens
+    if(!QueryPerformanceFrequency(&frequency)){
+        cout << "Error: Hardware doesn't support high performance" << endl;
+        return false;
+    }
+
+    /*if(frequency == 0){
+        cout << "Error: Hardware doesn't support high performance" << endl;
+        return false;
+    }*/
+
+    return true;
+}
--- src/background/precisetimer.h
+++ src/background/precisetimer.h
@@ -2,6 +2,13 @@
 #define PRECISETIMER_H
 
 #include <windows.h>
+#include <iostream>
+
+using namespace std;
+
+// constants to measure the time stamp
+#define START true      // time stamp saved in start
+#define STOP  false     // time stamp saved int stop
 
 // Measures with the windows api the time in ms accuracy.
 
@@ -9,14 +16,13 @@ class PreciseTimer
 {
 public:
     PreciseTimer();
-    bool startTimer();      // start the timer
-    bool stopTimer();       // stop the timer
+    bool init();
+    bool setTimeStamp(bool);
     long elapsed();         // calculates the differenz between start and stop and set them to 0
 private:
-    /*
-    TIME start;
-    TIME stop;
-    */
+    LARGE_INTEGER start;
+    LARGE_INTEGER stop;
+    LARGE_INTEGER frequency;
 };
 
 #endif // PRECISETIMER_H

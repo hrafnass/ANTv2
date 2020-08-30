de12c01369aa0920a95c9d2cf48a71b2c6933532
hrafnass <aetas.cieris@posteo.de>
2020-08-30 18:04:56 +0200

    changed the one reset function in 2

--- src/background/calculation.cpp
+++ src/background/calculation.cpp
@@ -1,23 +1,6 @@
 #include "calculation.h"
 
 // public methods
-void Calculation::ResetCalculation(){
-    // reset calculations
-    confict_effect = 0;
-    orientation_effect = 0;
-    alertnes_effect = 0;
-    median_rt = 0;
-    error_sum = 0;
-    // reset vectors
-    v_double_cue.clear();
-    v_no_cue.clear();
-    v_spatial_cue.clear();
-    v_center_cue.clear();
-    v_congruent.clear();
-    v_incongruent.clear();
-    v_rt.clear();
-}
-
 // Calculate all values from the measured values
 void Calculation::CalcValues(){
     // calculates the effects
@@ -27,6 +10,7 @@ void Calculation::CalcValues(){
     // calculates the median of the reaction time (all right reactions)
     median_rt = Median(&v_rt);
 }
+
 
 // private methods
 // calculates the Median from a vector of reaction times
@@ -39,6 +23,27 @@ float Calculation::Median(std::vector<int>* arg_v){
         return static_cast<float>(arg_v->at(pos));
     }
     return static_cast<float>((arg_v->at(pos-1) + arg_v->at(pos)) / 2); // the median with even vector size
+}
+
+// reset all vectors
+void Calculation::ResetVectors(){
+    // reset vectors
+    v_double_cue.clear();
+    v_no_cue.clear();
+    v_spatial_cue.clear();
+    v_center_cue.clear();
+    v_congruent.clear();
+    v_incongruent.clear();
+    v_rt.clear();
+}
+// reset the calculation
+void Calculation::ResetCalculation(){
+    // reset calculations
+    confict_effect = 0;
+    orientation_effect = 0;
+    alertnes_effect = 0;
+    median_rt = 0;
+    error_sum = 0;
 }
 
 // calculates an effect - differences of two medians

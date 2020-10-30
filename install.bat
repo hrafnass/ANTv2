ff014500938ab6fc62488ee63458fd2722e6ddb9
hrafnass <aetas.cineris@posteo.de>
2020-10-30 21:46:13 +0100

    added Copyright and license in install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,27 @@
-@echo off
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::
+:: Copyright (C) 2020 Siegfried Gauggel
+:: Copyright (C) 2020 Marcel Gräfenstein
+::
+:: This file is part of ANTv2.
+::
+:: ANTv2 is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU General Public License as published by
+:: the Free Software Foundation, either version 3 of the License, or
+:: (at your option) any later version.
+::
+:: ANTv2 is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+:: GNU General Public License for more details.
+::
+:: You should have received a copy of the GNU General Public License
+:: along with ANTv2.  If not, see <https://www.gnu.org/licenses/>.
+::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+
+@echo off
 
 REM Ask where the installed qt bin folder is
 set /p qt="Where did you installed Qt (qmake.exe) e.g. C:\Qt\5.12.2\mingw73_64\bin\, if you used the qt source code, given with the ANTv2 source code ? "
